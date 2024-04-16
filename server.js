825733c9b2684175be1d1bae217db13e652a1355
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-04-16 17:33:53 +0530

    Dfa

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ import authRoutes from "./routes/authRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import path from "path";
-
+import { fileURLToPath } from "url";
 // Configure environment variables
 dotenv.config();
 
-// Connect to the database
-connectDB().then(() => {
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
   // Create Express app
   const app = express();
 
   // Middleware setup
   app.use(cors());
   app.use(express.json());
-  app.use(express.static(path.join(process.cwd(), "client", "build")));
+  app.use(express.static(path.join(__dirname,'./client/build')));
+
+
+
+  app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname),'./client/build/index.html');
+  })
 
   // Routes
   app.use("/api/v1/product/", productRoutes);
@@ -39,6 +47,7 @@ connectDB().then(() => {
   // Port
   const PORT = process.env.PORT || 5000;
 
+connectDB().then(() => {
   // Start the server
   app.listen(PORT, () => {
     console.log(
