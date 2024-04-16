6890291c3ca53011f35984a624c5f0b17d56e9ba
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-04-16 17:14:10 +0530

    dfad

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,17 +1,16 @@
-const mongoose = require("mongoose");
-
+import mongoose from "mongoose";
+import colors from "colors";
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
+    const conn = await mongoose.connect(
       "mongodb+srv://arunsharma96025:8N5fNZzIIUIUeL3p@pdfeditor.lu4uirh.mongodb.net/?retryWrites=true&w=majority&appName=PdfEditor"
     );
     console.log(
-      `MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log("MongoDB conncetion FAILED", error);
-    process.exit(1);
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
