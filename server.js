56df34e47146aa516ececddf62ad43d8b1f504a9
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-04-16 20:08:40 +0530

    sever

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Middleware setup
-app.use(cors()); // Enable CORS for all origins during development
+app.use(
+  cors({
+    origin: "*", // Allow requests from any origin
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(morgan("dev")); // Logging middleware
@@ -39,20 +43,6 @@ app.use("/api/v1/category/", categoryRoutes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
-});
-
-// CORS error handling middleware
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    console.log("CORS preflight request received");
-    return res.sendStatus(200);
-  }
-
-  next();
 });
 
 // Port
