195c173047031689f10a7a20a3d99a5ecef8755d
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-04-16 18:52:08 +0530

    adf

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://arunsharma96025:8N5fNZzIIUIUeL3p@pdfeditor.lu4uirh.mongodb.net/?retryWrites=true&w=majority&appName=PdfEditor"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
