cc4869a0eb44c1efc70a16ec355f14dd84bd185b
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-04-16 21:54:20 +0530

    sever

--- server.js
+++ server.js
@@ -55,6 +55,11 @@ const PORT = process.env.PORT || 5000;
 // Connect to database and start server
 connectDB()
   .then(() => {
+    
+    app.use("/login", (req, res) => {
+      res.json("i am working g");
+    });
+    
     app.listen(PORT, () => {
       console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
